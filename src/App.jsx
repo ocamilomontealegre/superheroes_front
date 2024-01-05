1caa2b11d59fb4e069ffc6a348a74fdf3ede7d0d
Camilo Montealegre <ocamilomontealegre@gmail.com>
2024-01-04 20:01:16 -0500

    Add Container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Container from './components/Container.jsx';
 import './App.css'
 
-
+const App = () => {
   return (
     <>
-      <div>
-        
+      <Container />
     </>
   )
 }
---
+++ src/components/Container.jsx
@@ -0,0 +1,9 @@
+const Container = ({ children }) => {
+  return(
+    <div className='main-container'>
+      {children}
+    </div>
+  )
+};
+
+export default Container;
